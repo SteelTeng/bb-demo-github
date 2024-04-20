a51f9c0e4d885f8070314be28107916d051c73c3
steel <steeleteng@msn.cn>
2024-04-20 14:54:50 +0800

    添加项目文件。

---
+++ bb-demo-github/Data/WeatherForecast.cs
@@ -0,0 +1,28 @@
+﻿namespace bb_demo_github.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WeatherForecast
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int TemperatureC { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Summary { get; set; } = "";
+    }
+}
